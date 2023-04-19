bcb6ea69bccb7b1c66d8b58a8f383605de9bb6e7
Michael Singleton <msingleton2337@gmail.com>
2023-04-19 16:07:55 -0400

    feat: Altered broken test

--- src/article/service/main_test.go
+++ src/article/service/main_test.go
@@ -17,16 +17,21 @@ func TestConnectDB( t *testing.T ) {
 }
 
 func TestInsertDB( t *testing.T ) {
-  var database *sql.DB
-  var err error
-  var status int
+  //var database *sql.DB
+  //var err error
+  //var status int
 
-  var art Article = Article{ id: 1, author: "Mike", title: "TestTitle", description: "TestDesc", 
-                             keywords: []string{ "One", "Two" }, contents: "TestContents" }
+  // [TODO: Uncomment after solving runtime error
+  //var art Article = Article{ id: 1, author: "Mike", title: "TestTitle", description: "TestDesc", 
+  //                           keywords: []string{ "One", "Two" }, contents: "TestContents" }
 
-  status = insertDB( database, err, art )
+  //status = insertDB( database, err, art )
 
-  if ( status != 0 || err != nil ) {
-    t.Fatalf( `connectDB() Failed with error message: %s`, err )
-  }
+  //if ( status != 0 || err != nil ) {
+  //  t.Fatalf( `connectDB() Failed with error message: %s`, err )
+  //}
+  
+  // Tested function's failure may be linked to database issues
+  // Deliberately fail to avoid runtime error
+  t.Fatalf( `ConnectDB() failed with error message: panic: runtime error: invalid memory address or nil pointer dereference [recovered]` )
 }
